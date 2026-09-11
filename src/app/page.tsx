9f377fdd74db00c2162d271d3441fe612755d744
Felipe Elias Soares <felipe.soares@enterscience.com.br>
2026-09-11 13:14:52 -0300

    fix(page): add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learningaaaaaa
+              Learninga
             </a>{' '}
             center.
           </p>
